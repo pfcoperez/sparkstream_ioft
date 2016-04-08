b4d62c53708a89f9965b0807a137ba628f41d6b4
Pablo Fco. Pérez Hidalgo <contact@pablofranciscoperez.info>
2016-04-08 16:29:56 +0200

    Made simulator cyclic

--- samples/simulator.sh
+++ samples/simulator.sh
@@ -11,11 +11,12 @@ function getEntryTime {
     echo $1 | grep -Po "\"gcs_timestamp_ms\":[0-9]+" | awk -F ":" '{print $2}'  
 }
 
-ZERO_TIME=$(getEntryTime $(head -n 1 $1))
-
-while read json ; do
-    CUR_TIME=$(getEntryTime $json)
-    T=$(echo "($CUR_TIME - $ZERO_TIME)/1000.0" | bc -l)
-    sleep $T
-    echo $json
-done < $1 | nc -l -k 7891
+while [ 1 ]; do
+    ZERO_TIME=$(getEntryTime $(head -n 1 $1))
+    while read json ; do
+	CUR_TIME=$(getEntryTime $json)
+	T=$(echo "($CUR_TIME - $ZERO_TIME)/1000.0" | bc -l)
+	sleep $T
+	echo $json
+    done < $1
+done | nc -l -k 7891
