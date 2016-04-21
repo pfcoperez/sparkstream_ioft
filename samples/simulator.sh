68a9334396d9b6b1a5fd9dcc35be4fa5c36de389
Pablo Fco. Pérez Hidalgo <pfperez@stratio.com>
2016-04-21 17:57:18 +0200

    Fixed simulator scale

--- samples/simulator.sh
+++ samples/simulator.sh
@@ -12,12 +12,18 @@ function getEntryTime {
     echo $1 | grep -Po "\"gcs_timestamp_ms\":[0-9]+" | awk -F ":" '{print $2}'  
 }
 
+ZERO_TIME=$(getEntryTime $(head -n 1 $1))
+
 while [ 1 ]; do
-    ZERO_TIME=$(getEntryTime $(head -n 1 $1))
+    PREV_TIME=$ZERO_TIME
     while read json ; do
 	CUR_TIME=$(getEntryTime $json)
-	T=$(echo "($CUR_TIME - $ZERO_TIME)/1000.0" | bc -l)
-	sleep $T
+	T=$(echo "($CUR_TIME - $PREV_TIME)/1000.0" | bc -l | cut -c1-7)
+	SIGN=$(echo $T | cut -c1-1)
+	if [ $SIGN != "-" ]; then
+	    sleep $T;
+	    PREV_TIME=$CUR_TIME
+	fi;
 	echo $json
     done < $1
 done | nc -l -k 7891
