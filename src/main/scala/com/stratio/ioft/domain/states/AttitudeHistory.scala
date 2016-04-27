c6b719053003c48a0544482ba60313644a69cb8c
Pablo Fco. Pérez Hidalgo <pfperez@stratio.com>
2016-04-27 17:34:41 +0200

    Added method whereby the know attitutdes at some time are provided.

--- src/main/scala/com/stratio/ioft/domain/states/AttitudeHistory.scala
+++ src/main/scala/com/stratio/ioft/domain/states/AttitudeHistory.scala
@@ -6,4 +6,12 @@ case class AttitudeHistory(attitudes: Seq[(BigInt, Attitude)]) {
   def isEmpty: Boolean = attitudes.isEmpty
   def startsAt: Option[BigInt] = attitudes.headOption.map(_._1)
   def endsAt: Option[BigInt] = attitudes.lastOption.map(_._1)
+
+  def attitudeAt(timestamp_ms: BigInt): Seq[(BigInt, Attitude)] =
+    attitudes indexWhere { case (ts, _) => ts > timestamp_ms } match {
+      case -1 => Seq()
+      case 0 => Seq(attitudes.head)
+      case n => attitudes.slice(n-1, n+1)
+    }
+
 }
