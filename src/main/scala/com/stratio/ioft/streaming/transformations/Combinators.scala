8f3e7aad8f88aabef6ad7f00ba75863daedfa226
Pablo Fco. Pérez Hidalgo <pfperez@stratio.com>
2016-05-05 18:05:12 +0200

    Added desired vs actual attitude Combinator

--- src/main/scala/com/stratio/ioft/streaming/transformations/Combinators.scala
+++ src/main/scala/com/stratio/ioft/streaming/transformations/Combinators.scala
@@ -6,6 +6,9 @@ import com.stratio.ioft.domain.states.AttitudeHistory
 import com.stratio.ioft.util.Math.Geometry._
 import org.apache.spark.streaming.dstream.DStream
 import com.stratio.ioft.domain.measures.VectorMeasure._
+import com.stratio.ioft.streaming.transformations.Aggregators.attitudeHistoryStream
+
+import org.apache.spark.streaming.Milliseconds
 
 object Combinators {
 
@@ -13,7 +16,7 @@ object Combinators {
                                     accelerationStream: DStream[(DroneIdType, (BigInt, Acceleration))],
                                     attitudeHistoryStream: DStream[(DroneIdType, AttitudeHistory)]
                                   ): DStream[(DroneIdType, (BigInt, Acceleration))] =
-    accelerationStream.join(attitudeHistoryStream) flatMap {
+    accelerationStream join attitudeHistoryStream flatMap {
       case (id, ((ts, acceleration), attitudeFrameHistory)) =>
         val closestAttitudes = attitudeFrameHistory.attitudeAt(ts)
         closestAttitudes.headOption map { _ =>
@@ -23,5 +26,29 @@ object Combinators {
           id -> (ts -> Acceleration.tupled(rotate(attitude map (math.toRadians(_)), acceleration))) 
         }
     }
+
+  def desiredAndActualAttitudeStream(
+                                      desiredAttitudeStream: DStream[(DroneIdType, (BigInt, Attitude))],
+                                      attitudeStream: DStream[(DroneIdType, (BigInt, Attitude))],
+                                      timeRange: Long
+                                    ): DStream[(DroneIdType, (BigInt, Attitude, Attitude))] = {
+
+    def windowed(stream: DStream[(DroneIdType, (BigInt, Attitude))]) = {
+      val windowDuration = Milliseconds(timeRange)
+      stream.window(windowDuration, windowDuration)
+    }
+
+    windowed(desiredAttitudeStream) join attitudeHistoryStream(windowed(attitudeStream)) flatMap {
+      case (id, ( (ts, desired), actualAttitudeHistory)) =>
+        val closestAttitudes = actualAttitudeHistory.attitudeAt(ts)
+        closestAttitudes.headOption map { _ =>
+          val (_, actualAttitude: Attitude) = closestAttitudes.minBy {
+            case (actual_ts, _) => math.abs((actual_ts-ts).toLong)
+          }
+          id -> (ts, desired, actualAttitude)
+        }
+    }
+
+  }
 
 }
